466fe60cc4beca2d66a05f3aad09e384c37da020
Eirik Langholm Vullum <evullum@gmail.com>
2020-09-05 17:01:19 +0200

    Updates

---
+++ precache-manifest.72877e8d8e98392be192247d921ec20d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09da3a944c216e7507629d0e05c66ff2",
+    "url": "/kahoot-demo/index.html"
+  },
+  {
+    "revision": "f8839ed65bd853a0716e",
+    "url": "/kahoot-demo/static/css/main.dc032eab.chunk.css"
+  },
+  {
+    "revision": "3b7993f5af87f12f65c7",
+    "url": "/kahoot-demo/static/js/2.f72abaf2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kahoot-demo/static/js/2.f72abaf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8839ed65bd853a0716e",
+    "url": "/kahoot-demo/static/js/main.c565da92.chunk.js"
+  },
+  {
+    "revision": "34b5388d4eeb7d12dc11",
+    "url": "/kahoot-demo/static/js/runtime-main.1bac9878.js"
+  }
+]);
